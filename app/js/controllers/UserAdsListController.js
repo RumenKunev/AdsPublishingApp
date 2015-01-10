eecbf245db10e0da7c849999ba9b2afd410b49b3
RumenKunev <rumkunev@yahoo.com>
2015-01-11 00:39:44 +0200

    userAdsList.html

--- app/js/controllers/UserAdsListController.js
+++ app/js/controllers/UserAdsListController.js
@@ -7,27 +7,13 @@ angular.module("AdsPublisher")
             $scope.isLogged = authenticationData.isLogged();
 
             $scope.displayUsername = function displayUsername(){
-                var username = '';
+                var username = {};
                 if($scope.isLogged){
                     username = authenticationData.getAuthenticationToken().username;
                 }
                 return username;
             };
 
-            $scope.allTowns = adsData.getAllTowns();
-            $scope.allCategories = adsData.getAllCategories();
+            $scope.allUserAds = userAdsData.getUserAds();
 
-            $scope.publishAd = function publishAd(publishForm, publishAdForm){
-                if (publishAdForm.$valid) {
-                    userAdsData.publishUserAd(publishForm)
-                        .$promise
-                        .then(function (result) {
-                            notifyService.showInfo('Advertisement submitted for approval. Once approved, it will be published.');
-                            $location.path("/user/ads");
-                        });
-                } else {
-                    notifyService.showError("Publish ad failed advertisement title and text are required!", serverError);
-                }
-            };
-        }]
-);
+    }]);
